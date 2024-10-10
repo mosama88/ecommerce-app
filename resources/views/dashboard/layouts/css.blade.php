f9dd679731fa67f8418cb0f4aa9a5745fd078500
mosama88 <mosama88@hotmail.com>
2024-10-10 12:49:33 +0300

    Done Back2

---
+++ resources/views/dashboard/layouts/css.blade.php
@@ -0,0 +1,47 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description"
+    content="Multikart admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+<meta name="keywords"
+    content="admin template, Multikart admin template, dashboard template, flat admin template, responsive admin template, web app">
+<meta name="author" content="pixelstrap">
+<link rel="icon" href="{{ asset('dashboard') }}/assets/images/dashboard/favicon.png" type="image/x-icon">
+<link rel="shortcut icon" href="{{ asset('dashboard') }}/assets/images/dashboard/favicon.png" type="image/x-icon">
+<title>Multikart - @yield('title')</title>
+
+<!-- Google font-->
+<link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,500;1,600;1,700;1,800;1,900&display=swap">
+
+<link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap">
+
+
+<!-- Font Awesome-->
+<link rel="stylesheet" type="text/css" href="{{ asset('dashboard') }}/assets/css/vendors/font-awesome.css">
+
+<!-- Flag icon-->
+<link rel="stylesheet" type="text/css" href="{{ asset('dashboard') }}/assets/css/vendors/flag-icon.css">
+
+<!-- ico-font-->
+<link rel="stylesheet" type="text/css" href="{{ asset('dashboard') }}/assets/css/vendors/icofont.css">
+
+<!-- Prism css-->
+<link rel="stylesheet" type="text/css" href="{{ asset('dashboard') }}/assets/css/vendors/prism.css">
+
+<!-- Chartist css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('dashboard') }}/assets/css/vendors/chartist.css">
+
+<!-- Font Ar  font-family: DroidKufi-Regular;  -->
+<link rel="stylesheet" type="text/css" href="{{ asset('dashboard') }}/assets/fonts_ar/stylesheet.css">
+
+<!-- Bootstrap css-->
+<link rel="stylesheet" type="text/css" href="{{ asset('dashboard') }}/assets/css/vendors/bootstrap.css">
+
+<!-- App css-->
+<link rel="stylesheet" type="text/css" href="{{ asset('dashboard') }}/assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('dashboard') }}/assets/css/select2.min.css">
+
+
+@yield('css')
