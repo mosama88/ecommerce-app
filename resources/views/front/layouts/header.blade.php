ead8be2d7ee84f6e1f924b50c2f17eddaa6cbcfe
mosama88 <mosama88@hotmail.com>
2024-10-10 14:35:52 +0300

    front 4

---
+++ resources/views/front/layouts/header.blade.php
@@ -0,0 +1,72 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="main-menu">
+                <div class="menu-left">
+                    <div class="navbar">
+                        <a href="javascript:void(0)">
+                            <div class="bar-style"><i class="fa fa-bars sidebar-bar" aria-hidden="true"></i>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="brand-logo">
+                        <a href="index.html"><img src="{{ asset('front') }}/assets/images/icon/logo.png"
+                                class="img-fluid blur-up lazyload" alt=""></a>
+                    </div>
+                </div>
+                <div class="menu-right pull-right">
+                    <div>
+                        <nav>
+                            <div class="toggle-nav"><i class="fa fa-bars sidebar-bar"></i></div>
+                            <ul class="sm pixelstrap sm-horizontal">
+                                <li>
+                                    <div class="mobile-back text-end">Back<i class="fa fa-angle-right ps-2"
+                                            aria-hidden="true"></i></div>
+                                </li>
+                                <li>
+                                    <a href="index.html">Home</a>
+                                </li>
+                                <li>
+                                    <a href="#">feature<div class="lable-nav">new</div></a>
+                                </li>
+                                <li>
+                                    <a href="#">shop</a>
+                                </li>
+                                <li>
+                                    <a href="#">product</a>
+                                </li>
+                                <li>
+                                    <a href="#">pages</a>
+                                </li>
+                                <li><a href="#">blog</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div>
+                        <div class="icon-nav d-none d-sm-block">
+                            <ul>
+                                <li class="onhover-div mobile-search">
+                                    <div><img src="{{ asset('front') }}/assets/images/icon/search.png"
+                                            onclick="openSearch()" class="img-fluid blur-up lazyload"
+                                            alt=""> <i class="ti-search"
+                                            onclick="openSearch()"></i></div>
+                                </li>
+                                <li class="onhover-div mobile-setting">
+                                    <div><img src="{{ asset('front') }}/assets/images/icon/setting.png"
+                                            class="img-fluid blur-up lazyload" alt=""> <i
+                                            class="ti-settings"></i></div>
+                                </li>
+                                <li class="onhover-div mobile-cart">
+                                    <div><img src="{{ asset('front') }}/assets/images/icon/cart.png"
+                                            class="img-fluid blur-up lazyload" alt=""> <i
+                                            class="ti-shopping-cart"></i></div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
