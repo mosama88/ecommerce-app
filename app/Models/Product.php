441bca6fd27f1b3a5b4674775780b5103497dbfd
mosama88 <mosama88@hotmail.com>
2024-10-14 21:04:54 +0300

    well Done 4

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,48 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 class Product extends Model
 {
     use HasFactory;
 protected $table ='products';
 
 protected $guarded = [];
+
+
+   public function image(): MorphOne
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+    
+    public function createdBy()
+    {
+        return $this->belongsTo(Admin::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(Admin::class, 'updated_by');
+    }
+
+       public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+         public function size()
+    {
+        return $this->belongsTo(Size::class, 'size_id');
+    }
+
+          public function brand()
+    {
+        return $this->belongsTo(Brand::class, 'brand_id');
+    }
+
+            public function color()
+    {
+        return $this->belongsTo(Color::class, 'color_id');
+    }
+
 }
---
+++ app/Traits/UploadTrait.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+use App\Models\Image;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+trait UploadTrait
+{
+
+    public function verifyAndStoreImage(Request $request, $inputname, $foldername, $disk, $imageable_id, $imageable_type)
+    {
+
+        if ($request->hasFile($inputname)) {
+
+            // Check img
+            if (!$request->file($inputname)->isValid()) {
+                // flash('Invalid Image!')->error()->important();
+                session()->flash('message', 'Invalid Image!');
+                return redirect()->back()->withInput();
+            }
+
+            $photo = $request->file($inputname);
+            $name = Str::slug($request->input('name'));
+            $filename = $name . '.' . $photo->getClientOriginalExtension();
+
+            // insert Image
+            $Image = new Image();
+            $Image->filename = $filename;
+            $Image->imageable_id = $imageable_id;
+            $Image->imageable_type = $imageable_type;
+            $Image->save();
+            return $request->file($inputname)->storeAs($foldername, $filename, $disk);
+        }
+
+        return null;
+    }
+
+
+
+    //For Vacation
+    public function verifyAndStoreFile(Request $request, $inputname, $foldername, $disk, $imageable_id, $imageable_type)
+    {
+
+        if ($request->hasFile($inputname)) {
+
+            // Check img
+            if (!$request->file($inputname)->isValid()) {
+                // flash('Invalid Image!')->error()->important();
+                session()->flash('message', 'Invalid Image!');
+                return redirect()->back()->withInput();
+            }
+
+            $photo = $request->file($inputname);
+            $name = Str::slug($request->input('type'));
+            $filename = $name . time() . '_' . rand(1000, 9999) . '.' . $photo->getClientOriginalExtension();
+
+            // insert Image
+            $Image = new Image();
+            $Image->filename = $filename;
+            $Image->imageable_id = $imageable_id;
+            $Image->imageable_type = $imageable_type;
+            $Image->save();
+            return $request->file($inputname)->storeAs($foldername, $filename, $disk);
+        }
+
+        return null;
+    }
+
+
+
+    public function Delete_attachment($disk, $path, $imageId)
+    {
+        // حذف الصورة من التخزين
+        if (Storage::disk($disk)->exists($path)) {
+            Storage::disk($disk)->delete($path);
+        }
+
+        // حذف سجل الصورة من قاعدة البيانات
+        Image::where('id', $imageId)->delete();
+    }
+}
+
+
+    // public function verifyAndStoreImageForeach($varforeach , $foldername , $disk, $imageable_id, $imageable_type) {
+
+    //     // insert Image
+    //     $Image = new Image();
+    //     $Image->filename = $varforeach->getClientOriginalName();
+    //     $Image->imageable_id = $imageable_id;
+    //     $Image->imageable_type = $imageable_type;
+    //     $Image->save();
+    //     return $varforeach->storeAs($foldername, $varforeach->getClientOriginalName(), $disk);
+    // }
