7d91da63965330b7c4e8cdd5454967f59ad33f6d
mosama88 <mosama88@hotmail.com>
2024-10-14 21:00:09 +0300

    well Done 3

--- database/migrations/2024_10_10_182042_create_products_table.php
+++ database/migrations/2024_10_10_182042_create_products_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title',300);
+            $table->text('description',3000);
+            $table->decimal('price',10,2);
+            $table->integer('qty');
+            $table->foreignId('category_id')->nullable()->references('id')->on('categories')->onUpdate('cascade');
+            $table->foreignId('size_id')->nullable()->references('id')->on('sizes')->onUpdate('cascade');
+            $table->foreignId('color_id')->nullable()->references('id')->on('colors')->onUpdate('cascade'); 
+            $table->foreignId('brand_id')->nullable()->references('id')->on('brands')->onUpdate('cascade'); 
+            $table->foreignId('created_by')->references('id')->on('admins')->onUpdate('cascade');
+            $table->foreignId('updated_by')->nullable()->references('id')->on('admins')->onUpdate('cascade');
             $table->timestamps();
         });
     }
